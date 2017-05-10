239b4d859cfe5519a49031e12e8e740d01c7ed05
Christoph Rieger <rieger.chris@googlemail.com>
2017-05-10 13:36:43 +0200

    add remote annotation

--- FoodDeliveryX-EJB/ejbModule/de/wwu/pi/fooddelivery/ejb/AddressService.java
+++ FoodDeliveryX-EJB/ejbModule/de/wwu/pi/fooddelivery/ejb/AddressService.java
@@ -1,7 +1,10 @@
 package de.wwu.pi.fooddelivery.ejb;
 
+import javax.ejb.Remote;
+
 import de.wwu.pi.fooddelivery.jpa.Address;
 
+@Remote
 public interface AddressService extends EntityService<Address> {
 
 }
